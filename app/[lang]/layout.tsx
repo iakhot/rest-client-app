22b76ccfb36719e7b56b304ffc8b168cba24e990
FX506LL\masan <locust13region@gmail.com>
2025-09-05 18:19:35 +0300

    chore: setup Mui

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { NextIntlClientProvider } from 'next-intl';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import ClientThemeProvider from './theme-provider';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -25,8 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+      <body>
+        <NextIntlClientProvider>
+          <ClientThemeProvider>{children}</ClientThemeProvider>
+        </NextIntlClientProvider>
       </body>
     </html>
   );
---
+++ app/[lang]/theme-provider.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import theme from '@/theme';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+export default function ClientThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={roboto.variable}>
+      <AppRouterCacheProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </AppRouterCacheProvider>
+    </div>
+  );
+}
