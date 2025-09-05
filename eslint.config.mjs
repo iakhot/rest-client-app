a5db4b84378a98f6e24ca42d7fc261cc6d207993
FX506LL\masan <locust13region@gmail.com>
2025-09-05 16:17:58 +0300

    init: setup Next & devDependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,53 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-plugin-prettier';
+import nextPlugin from '@next/eslint-plugin-next';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['node_modules/**', '.next/**', 'dist/**'],
+  },
+
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      '@typescript-eslint': tseslint.plugin,
+      prettier,
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-img-element': 'warn',
+      '@next/next/no-html-link-for-pages': ['warn', './app'],
+      '@next/next/no-head-import-in-document': 'warn',
+      '@next/next/next-script-for-ga': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
